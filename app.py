764374a510d8b94fde212f05ed446532000da356
Jesse Fulcher <jfulch@github.com>
2025-05-27 14:01:51 -0700

    chaning port

--- app.py
+++ app.py
@@ -237,5 +237,5 @@ if __name__ == '__main__':
         )
     else:
         # Production/deployment settings
-        port = int(os.environ.get('PORT', 10000))
+        port = int(os.environ.get('PORT', 8000))
         app.run(host='0.0.0.0', port=port)
